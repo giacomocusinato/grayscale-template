c12e8648799203057c0bdccc1fa51724359b06e9
giacomocusinato <giacomo.cusinato@outlook.com>
2016-03-14 21:46:25 +0100

    Gulp build set up

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,38 @@
 
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
-    inject = require('gulp-inject'),
     serve = require('gulp-serve');
 
 var paths = {
 	sass: './public/assets/sass/',
-    js: ['./public/vendor/angular/angular.js', './public/js/*.js'],
+    js: [
+    	'./public/vendor/jquery/dist/jquery.min.js',
+	 	'./public/vendor/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+	 	'./public/vendor/jquery.easing/js/jquery.easing.min.js',
+     	'./public/js/*.js'
+    ],
 	bower: './public/vendor/',
     cssDist: './dist/css/*css',
     jsDist: ['./dist/js/*.js', './dist/js/**/*.js']
 }
 
-var injectOptions = {
-    ignorePath: 'dist',
-    addRootSlash: false
-}
-
 gulp.task('css', function() {
-    return gulp.src(paths.sass + 'main.scss')
+    return gulp.src(paths.sass + 'grayscale.scss')
         .pipe(sass({
-            includePaths: [paths.bower + 'bootstrap-sass/assets/stylesheets'],
+            includePaths: [paths.bower + 'bootstrap-sass/assets/stylesheets', 
+            	paths.bower + 'font-awesome/scss'],
         }))
         .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('fonts', function() {
+	return gulp.src(['./public/assets/fonts/*', './public/vendor/font-awesome/fonts/*'])
+		.pipe(gulp.dest('./dist/fonts'));
+});
+
+gulp.task('images', function() {
+	return gulp.src('./public/assets/img/*')
+		.pipe(gulp.dest('./dist/img'));
 });
 
 gulp.task('js', function() {
@@ -31,14 +41,12 @@ gulp.task('js', function() {
         .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('inject', ['css', 'js'], function() {
+gulp.task('html', ['css', 'js'], function() {
 	return gulp.src('public/index.html')
-		.pipe(inject(gulp.src(paths.cssDist, {read: false}), injectOptions))
-        .pipe(inject(gulp.src(paths.jsDist, {read: false}), injectOptions))
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('build', ['inject']);
+gulp.task('build', ['html', 'css', 'js', 'fonts', 'images']);
 
 gulp.task('serve', ['build'], serve('./dist'));
 
